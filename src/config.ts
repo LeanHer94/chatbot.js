f33b98b9186731ccad9e3b4ba77b454937883dc3
Leandro Herenu <lherenu@coalfire.com>
2021-04-29 19:43:27 -0300

    adding express configuration

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,11 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const chatbotApiPort = process.env.API_PORT
-const chatbotApi = `${process.env.CHATBOT_API}:${chatbotApiPort}`
-const apiRetry = process.env.API_RETRY
-const apiRetryTime = process.env.API_RETRY_MILLISECONDS
+const port = process.env.PORT;
+const chatbotApi = `${process.env.CHATBOT_API}:${process.env.API_PORT}`;
+const apiRetry = process.env.API_RETRY;
+const apiRetryTime = process.env.API_RETRY_MILLISECONDS;
+const maxChannels = process.env.MAX_CHANNELS;
 
-export {
-    chatbotApi,
-    chatbotApiPort,
-    apiRetry,
-    apiRetryTime
-}
+export { chatbotApi, port, apiRetry, apiRetryTime, maxChannels };
