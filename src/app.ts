e392d6326d5ff181200d99ec15f77dffbd13c613
Leandro Herenu <lherenu@coalfire.com>
2021-04-29 19:43:59 -0300

    adding endpoints that allow us configure the bot remotedly

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
+import express from "express";
 import { AppError } from "error-api.hl/lib";
-import { chatbotApi } from './config';
+import { port, chatbotApi, maxChannels } from "./config";
 
 const api = chatbotApi;
-const irc = require('irc');
+const irc = require("irc");
 
 const ircConfig = {
-  server: 'irc.freenode.net',
+  server: "irc.freenode.net",
   port: 6667,
-  nick: 'LxbTimeZoneGuru',
-  channel: '#lxbuniquekdskds'
-}
+  nick: "hl-timezone-guru"
+};
 
 type Function = (timezone: string) => Promise<any>;
 type Message = (timezone: string, result: string | number) => string;
@@ -21,41 +21,82 @@ interface Command {
 
 const commands: { [id: string]: Command } = {
   "!timeat": {
-    fn: async (tz) => { return await axios.post(`${api}/timeat`, { Timezone: tz }) },
-    msg: (tz, result)  => result instanceof AppError ? result.description : `Current time at ${tz} is ${result}`
+    fn: async tz => {
+      return await axios.post(`${api}/timeat`, { Timezone: tz });
+    },
+    msg: (tz, result) =>
+      result instanceof AppError
+        ? result.description
+        : `Current time at ${tz} is ${result}`
   },
   "!timepopularity": {
-    fn: async (tz) => { return await axios.post(`${api}/timepopularity`, { Timezone: tz }) },
+    fn: async tz => {
+      return await axios.post(`${api}/timepopularity`, { Timezone: tz });
+    },
     msg: (tz, result) => `${tz} has been searched ${result} times`
   },
   "!commands": {
-    fn: async () => { return Promise.resolve({ data: "!timeat and !timepopularity" }) },
+    fn: async () => {
+      return Promise.resolve({ data: "!timeat and !timepopularity" });
+    },
     msg: (tz, result) => `Available commands are: ${result}`
   }
-}
+};
 
-var client = new irc.Client(
-  `${ircConfig.server}`, 
-  ircConfig.nick, 
-  {
-    channels: [ircConfig.channel], 
-    port: ircConfig.port,
-    retryCount: 3
-  });
+const client = new irc.Client(`${ircConfig.server}`, ircConfig.nick, {
+  port: ircConfig.port,
+  retryCount: 3
+});
 
-client.addListener('message', async function (from: any, to: any, message: string) {
-  const words = message.split(' ');
-  const command = words[0];
+const app = express();
+const mxChannels = maxChannels ? +maxChannels : 1;
 
-  if (commands[command]) {
-    client.say(ircConfig.channel, 'Looking for your request...');
+app.get("/join/:channel", async (req, res, next) => {
+  if ((client.opt.channels.length as number) == mxChannels) {
+    res.send(
+      `You cannot connect to more than ${mxChannels}. Use switch/:oldchannel/:newchannel to switch.`
+    );
 
-    const timezone = words[1];
-    const result = await commands[command].fn(timezone);
-    const message = commands[command].msg(timezone, result.data);
-
-    client.say(ircConfig.channel, message);
-  } else {
-    client.say(ircConfig.channel, 'You idiot');
+    return;
   }
+
+  const channel = `#${req.params.channel}`;
+  join(res, channel);
+});
+
+app.get("/switch/:oldchannel/:newchannel", async (req, res, next) => {
+  const oldchannel = `#${req.params.oldchannel}`;
+  const newchannel = `#${req.params.newchannel}`;
+  const message = `I'm living this shit but you can find me on ${newchannel}`;
+
+  client.part(oldchannel, message, () => {
+    join(res, newchannel);
+  });
+});
+
+const join = (res: any, channel: string) => {
+  client.join(channel, () => {
+    client.addListener("message", async function (from: any, to: any, message: string) {
+      const words = message.split(" ");
+      const command = words[0];
+
+      if (commands[command]) {
+        client.say(channel, "Looking for your request...");
+
+        const timezone = words[1];
+        const result = await commands[command].fn(timezone);
+        const message = commands[command].msg(timezone, result.data);
+
+        client.say(channel, message);
+      } else {
+        client.say(channel, "That's not an available command you idiot");
+      }
+    });
+
+    res.send(`${ircConfig.nick} joined to channel ${channel}.`);
+  });
+};
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
